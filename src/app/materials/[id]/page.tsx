88db448bc9b6305931eb5675b26e9f4f031b2da9
Bartosz <bartosz.janiuk@tsh.io>
2023-11-18 23:40:56 +0100

    disable ssg

--- src/app/materials/[id]/page.tsx
+++ src/app/materials/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { SingleMaterialView } from "@/_views/materials/components/singleMaterialView/SingleMaterialView";
+import { getBooksWithoutDetails } from "@/utils/api/book/getBooksWithoutDetails";
 import { getCurrentPage } from "@/utils/api/book/getCurrentPage";
 import { getBooks } from "@/utils/getBooks";
 import React from "react";
 
-export const generateStaticParams = async () => {
-	const books = await getBooks();
-	return books.map((book) => ({
-		id: book.id,
-	}));
-};
+// export const generateStaticParams = async () => {
+// 	const books = await getBooksWithoutDetails();
+// 	return books.map((book) => ({
+// 		id: book.id,
+// 	}));
+// };
 
 type MaterialPageProps = {
 	params: {
