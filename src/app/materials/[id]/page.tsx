44d5cbf54d424055e5f81596eea8b37f9508934e
Bartosz <bartosz.janiuk@tsh.io>
2023-11-18 22:34:12 +0100

    test

--- src/app/materials/[id]/page.tsx
+++ src/app/materials/[id]/page.tsx
@@ -10,7 +10,7 @@ export const generateStaticParams = async () => {
 	}));
 };
 
-// export const dynamicParams = true;
+export const dynamic = "force-dynamic";
 
 type MaterialPageProps = {
 	params: {
