0f09e2524a9a22b4ffdb15cb7b1b70c37b5c4794
Bartosz <bartosz.janiuk@tsh.io>
2023-11-19 11:44:33 +0100

    adjust book screen

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -17,7 +17,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			{isWithoutLayout && (
-				<div className="flex">
+				<div className="flex pb-[100px] md:pb-0 h-full w-full">
 					<Sidebar isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
 					<ContentWrapper isOpen={isOpen}>{children}</ContentWrapper>
 				</div>
